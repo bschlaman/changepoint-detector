add example data and fix bugs with log likelihood calculation

src/changepoint_detector/modules/detection/entry.py
@@ -5,7 +5,7 @@ import prettytable
 import changepoint_detector.utils.data_load
 import changepoint_detector.modules.detection.controller
 from changepoint_detector.modules.detection import strategy
-from bpyutils.formatting.colors import bld, yel
+from bpyutils.formatting.colors import bld, yel, dim
 
 log = logging.getLogger(__name__)
 
@@ -32,6 +32,7 @@ def run(path: str):
 
     controller = changepoint_detector.modules.detection.controller.Controller()
 
+    log.info(f"fitting {len(controller.strategies)} models...")
     res = controller.process_sync(df["logret"].to_numpy())
     metric_aic = controller.calculate_metric_aic(df["logret"].to_numpy())
     metric_bic = controller.calculate_metric_bic(df["logret"].to_numpy())
@@ -45,6 +46,8 @@ def preprocess(df: pd.DataFrame):
     df.rename(columns={security: "price"}, inplace=True)
     df["logret"] = df.apply(np.log).diff()
     df.dropna(inplace=True)
+    mask = df["logret"].abs() <= 0.0001
+    df.drop(df[mask].index, inplace=True)
 
 
 def display_res(
@@ -62,4 +65,8 @@ def display_res(
     pt.align = "l"
     for strat, n_cpts in num_changepoints.items():
         pt.add_row([yel(_fmt_name(strat)), n_cpts, aic[strat], bic[strat]])
+    pt.add_rows([
+        [dim("ChangepointMethods (TODO)"), "-", "-","-"],
+        [dim("BayesianMethods (TODO)"), "-", "-","-"],
+    ])
     print(pt)

src/changepoint_detector/modules/detection/strategy.py
@@ -50,7 +50,7 @@ class OneRegimePerQuarter(ChangepointDetectStrategy):
         self.cpts = (
             np.arange(len(ts) // self.TRADING_DAYS_PER_QUARTER)
             * self.TRADING_DAYS_PER_QUARTER
-        )
+        )[1:]
         return self.cpts
 
     @property
@@ -68,7 +68,7 @@ class OneRegimePerQuarter(ChangepointDetectStrategy):
         ll = 0
         for i, cpt in enumerate(cpts):
             segment = ts[0 if i == 0 else cpts[i - 1] : cpt]
-            ll += scipy.stats.norm(segment.mean(), segment.std()).logpdf(segment).sum()
+            ll += scipy.stats.norm.logpdf(segment, loc=segment.mean(), scale=segment.std(ddof=1)).sum()
         return ll
 
 
@@ -79,7 +79,7 @@ class RandChangepoints(ChangepointDetectStrategy):
 
     def __call__(self, ts: np.ndarray) -> np.ndarray:
         assert self.n < len(ts)
-        return np.random.randint(0, len(ts), self.n)
+        return np.sort(np.random.randint(0, len(ts), self.n))
 
     @property
     def display_name(self):
@@ -96,7 +96,7 @@ class RandChangepoints(ChangepointDetectStrategy):
         ll = 0
         for i, cpt in enumerate(cpts):
             segment = ts[0 if i == 0 else cpts[i - 1] : cpt]
-            ll += scipy.stats.norm(segment.mean(), segment.std()).logpdf(segment).sum()
+            ll += scipy.stats.norm.logpdf(segment, loc=segment.mean(), scale=segment.std(ddof=1)).sum()
         return ll
 
 
@@ -126,8 +126,8 @@ class AMOC(ChangepointDetectStrategy):
         assert self.changepoint != -1
         seg0, seg1 = ts[: self.changepoint], ts[self.changepoint :]
         return (
-            scipy.stats.norm(ts.mean(), ts.std()).logpdf(seg0).sum()
-            + scipy.stats.norm(ts.mean(), ts.std()).logpdf(seg1).sum()
+            scipy.stats.norm.logpdf(seg0, loc=ts.mean(), scale=ts.std(ddof=1)).sum()
+            + scipy.stats.norm.logpdf(seg1, loc=ts.mean(), scale=ts.std(ddof=1)).sum()
         )
 
 
@@ -194,9 +194,9 @@ class LikelihoodRatioMethod(ChangepointDetectStrategy):
 
     def __call__(self, ts: np.ndarray) -> np.ndarray:
         df = self._preprocess(ts)
-        cpts, _ = algo_changepoints.pelt(df, pen_=3, minseg=1, method="mbic_meanvar")
+        cpts, _ = algo_changepoints.pelt(df, pen_=3, minseg=2, method="mbic_meanvar")
         log.debug(f"[LRM] found {len(cpts)} changepoints")
-        self.cpts = cpts
+        self.cpts = np.sort(cpts)
         return cpts
 
     def get_n_params(self) -> int:
@@ -211,7 +211,7 @@ class LikelihoodRatioMethod(ChangepointDetectStrategy):
         ll = 0
         for i, cpt in enumerate(cpts):
             segment = ts[0 if i == 0 else cpts[i - 1] : cpt]
-            ll += scipy.stats.norm(segment.mean(), segment.std()).logpdf(segment).sum()
+            ll += scipy.stats.norm.logpdf(segment, loc=segment.mean(), scale=segment.std(ddof=1)).sum()
         return ll
 
 